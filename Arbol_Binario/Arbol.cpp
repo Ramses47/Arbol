5740470caeb3be3604a9a27859fd81f85c9acdea
Ramses47 <107227158+Ramses47@users.noreply.github.com>
2024-11-28 17:09:33 -0600

    nuevas inpreciones

--- Arbol_Binario/Arbol.cpp
+++ Arbol_Binario/Arbol.cpp
@@ -14,6 +14,9 @@ Nodo *crearNodo(int);
 void insertarNodo(Nodo *&, int);
 Nodo *arbol = NULL;
 void mostrarArbol(Nodo*, int);
+void preorden(Nodo*);
+void inorden(Nodo*);
+void postorden(Nodo*);
 
 
 int main() {
@@ -23,35 +26,52 @@ int main() {
 }
 //Menu
 void menu() {
-	int dato, opcion, contador=0;
-	do {
-		std::cout << "MENU" << std::endl;
-		std::cout << "1. Insertar nuevo nodo" << std::endl;
-		std::cout << "2. Mostar Arbol" << std::endl;
-		std::cout << "3. Salir" << std::endl;
-		std::cin >> opcion;
+    int dato, opcion, contador = 0;
+    do {
+        std::cout << "MENU" << std::endl;
+        std::cout << "1. Insertar nuevo nodo" << std::endl;
+        std::cout << "2. Mostrar Arbol (derecha a izquierda)" << std::endl;
+        std::cout << "3. Recorrido en Preorden" << std::endl;
+        std::cout << "4. Recorrido en Inorden" << std::endl;
+        std::cout << "5. Recorrido en Postorden" << std::endl;
+        std::cout << "6. Salir" << std::endl;
+        std::cin >> opcion;
 
-		switch (opcion)
-		{
-		case 1: std::cout << "Dame un numero:" << std :: endl;
-			std::cin >> dato;
-			insertarNodo(arbol, dato);
-			std::cout << "" << std::endl;
-			break;
-
-		case 2:
-			std::cout << "Mostrar arbol" << std::endl;
-			mostrarArbol(arbol, contador);
-			std::cout << "" << std::endl;
-			break;
-		case 3:
-			std::cout << "Saliendo" << std::endl;
-			break;
-		default:
-			std::cout << "opcion no valida" << std::endl;
-			break;
-		}
-	} while (opcion != 3);
+        switch (opcion) {
+        case 1:
+            std::cout << "Dame un numero:" << std::endl;
+            std::cin >> dato;
+            insertarNodo(arbol, dato);
+            std::cout << "" << std::endl;
+            break;
+        case 2:
+            std::cout << "Mostrar arbol:" << std::endl;
+            mostrarArbol(arbol, contador);
+            std::cout << "" << std::endl;
+            break;
+        case 3:
+            std::cout << "Recorrido en Preorden:" << std::endl;
+            preorden(arbol);
+            std::cout << "" << std::endl;
+            break;
+        case 4:
+            std::cout << "Recorrido en Inorden:" << std::endl;
+            inorden(arbol);
+            std::cout << "" << std::endl;
+            break;
+        case 5:
+            std::cout << "Recorrido en Postorden:" << std::endl;
+            postorden(arbol);
+            std::cout << "" << std::endl;
+            break;
+        case 6:
+            std::cout << "Saliendo..." << std::endl;
+            break;
+        default:
+            std::cout << "Opcion no valida" << std::endl;
+            break;
+        }
+    } while (opcion != 6);
 }
 
 //Funcion para crear un nuevo nodo
@@ -95,4 +115,33 @@ void mostrarArbol(Nodo* arbol, int cont) {
 		std::cout << arbol->dato << std::endl;
 		mostrarArbol(arbol->izq, cont + 1);
 	}
+}
+// Recorrido en Preorden
+void preorden(Nodo* arbol) {
+    if (arbol == NULL) {
+        return;
+    }
+    std::cout << arbol->dato << " ";
+    preorden(arbol->izq);
+    preorden(arbol->der);
+}
+
+// Recorrido en Inorden
+void inorden(Nodo* arbol) {
+    if (arbol == NULL) {
+        return;
+    }
+    inorden(arbol->izq);
+    std::cout << arbol->dato << " ";
+    inorden(arbol->der);
+}
+
+// Recorrido en Postorden
+void postorden(Nodo* arbol) {
+    if (arbol == NULL) {
+        return;
+    }
+    postorden(arbol->izq);
+    postorden(arbol->der);
+    std::cout << arbol->dato << " ";
 }
